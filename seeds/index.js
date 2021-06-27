d71f546d2e8aeb94913f06bbd114ef562e7ab51f
Chris Abiva <chrisabiva@hotmail.com>
2021-06-27 08:21:39 -0700

    Added more data in User Seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { User, Trail, Trail_Category } = require('../models');
+// const { User, Trail, Trail_Category } = require('../models');
 
 const seedUser = require('./user-seed.js');
 const seedTrail = require('./trail-seed.js');
@@ -8,19 +8,22 @@ const seedTrailCategory = require('./trail-category-seed.js');
 const seedDatabase = async () => {
    try {
     await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
     // await seedTrailCategory();
     await seedTrail();
+    console.log('\n----- TRAIL SEEDED -----\n');
     await seedUser();
-    
+    console.log('\n----- USER SEEDED -----\n');
     // const user = await User.bulkCreate(userData, {
     //     individualHooks: true,
     //     return: true,
     // });
+    process.exit(0);
    } catch (error) {
        console.log(error)
    }
 
-    process.exit(0);
+    
 
     }
 
--- seeds/user-seed.js
+++ seeds/user-seed.js
@@ -11,10 +11,28 @@ const userData = [
         trails_id: 1
     },
     {
-        username: 'someone',
-        name: 'someone',
-        email: 'someone@hotmail.com',
-        password: 'someone',
+        username: 'vinette',
+        name: 'vinette',
+        email: 'vinette@hotmail.com',
+        password: 'password',
+        access_level: 1,
+        date_created: '2021-06-22',
+        trails_id: 1
+    },
+    {
+        username: 'rex',
+        name: 'rex',
+        email: 'rex@hotmail.com',
+        password: 'password',
+        access_level: 1,
+        date_created: '2021-06-22',
+        trails_id: 1
+    },
+    {
+        username: 'matt',
+        name: 'matt',
+        email: 'matt@hotmail.com',
+        password: 'matt',
         access_level: 1,
         date_created: '2021-06-22',
         trails_id: 1
