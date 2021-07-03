c23a1ed3230a3ffd0906997d299872855a49b57f
Rex Yu <yu19910513@gmail.com>
2021-07-02 19:13:20 -0700

    update

--- public/javascript/add-hike.js
+++ public/javascript/add-hike.js
@@ -7,14 +7,7 @@ async function newFormHandler(event) {
     const data_been_there = document.querySelector(".post-date").value
     const category = document.querySelector(".post-category").value
     const difficulty = document.querySelector(".post-difficulty").value
-    var season;
-    if (document.getElementById("#summer").checked = true) {
-      season = "summer"
-    } else if (document.getElementById("#spring").checked = true){
-      season = "spring"
-    } else if (document.getElementById("#winter").checked = true){
-      season = "winter"
-    } else { season = "autumn"}
+    const season = document.querySelector(".post-season").value
 
 
     const response = await fetch(`/api/trail`, {
@@ -40,4 +33,40 @@ async function newFormHandler(event) {
     }
   }
 
-  document.querySelector('.add-hike-form').addEventListener('submit', newFormHandler);
+
+  function rex() {
+    const name = document.querySelector("#post-title").value;
+    const location = document.querySelector("#post-location").value
+    const zip = document.querySelector("#post-zip").value
+    const data_been_there = document.querySelector("#post-date").value
+    let season = document.querySelector(".post-season").value;
+    let difficulty = document.querySelector(".post-difficulty").value
+
+     //Create an Array.
+     var selected = new Array();
+
+     //Reference the Table.
+     var tblFruits = document.getElementById("tblFruits");
+
+     //Reference all the CheckBoxes in Table.
+     var chks = tblFruits.getElementsByTagName("INPUT");
+
+     // Loop and push the checked CheckBox value in Array.
+     for (var i = 0; i < chks.length; i++) {
+         if (chks[i].checked) {
+             selected.push(chks[i].value);
+         }
+     }
+
+     //Display the selected CheckBox values.
+     if (selected.length > 0) {
+         alert("Selected values: " + selected.join(","));
+     }
+
+    console.log(name);
+    console.log(location);
+    console.log(zip);
+    console.log(data_been_there);
+    console.log(season);
+    console.log(difficulty);
+  }
