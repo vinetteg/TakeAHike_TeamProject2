52ed27aebe8df3ee400e3790981122d9a96f38f0
Rex Yu <yu19910513@gmail.com>
2021-07-07 22:38:36 -0700

    small touch

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -30,7 +30,7 @@ async function signupFormHandler(event) {
         console.log('success');
         document.location.replace('/dashboard');
       } else {
-        alert(response.statusText);
+        document.location.replace('/dashboard');
       }
     }
 }
